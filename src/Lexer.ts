c3304fe5ef96c97ec0a861efc9f8919b12a4a776
hante <hante@youzan.com>
2017-05-16 10:30:39 +0800

    fix >

--- src/Lexer.ts
+++ src/Lexer.ts
@@ -1,16 +1,15 @@
 export default class Lexer {
   symbols: Array<string>
   grammar: Array<Object>
-  selfCloseTags: Array<string>
   constructor() {
     this.symbols = ['"', "'", '<', '>', '/'];
     this.grammar = [
-      { token: 'self-closing-tag', rule: /<([a-zA-Z])+\s*([^<>]*(=".*"))*?\s*\/>/ },
-      { token: 'start-tag', rule: /<([a-zA-Z])+\s*([^<>]*(=".*"))*?\s*>/ },
+      { token: 'self-closing-tag', rule: /<([a-zA-Z])+\s*([^<>"']|"[^"]*"|'[^']*')*?\s*\/>/ },
+      { token: 'start-tag', rule: /<([a-zA-Z])+\s*([^<>"']|"[^"]*"|'[^']*')*?\s*>/ },
       { token: 'end-tag', rule: /<\/([a-zA-Z])+\s*>/ },
       { token: 'comment', rule: /<!--.+?-->/ },
-      // TODO: change rule of string, support < and >
-      { token: 'string', rule: /[^<>]+/ }
+      // TODO: change rule of string, support <
+      { token: 'string', rule: /[^<>]+|[^<>]*>(?!<[a-zA-Z])*|<(?=<)/ }
     ];
   }
 
@@ -23,6 +22,7 @@ export default class Lexer {
     const grammarLen: Number = this.grammar.length;
     let tokens: Array<Object> = [];
     let token: Object;
+
     while (line.length > 0) {
       token = { index: line.length };
       for (let i = 0; i < grammarLen; i += 1) {
@@ -36,12 +36,21 @@ export default class Lexer {
         }
       }
       line = line.substring(token['index'] + token[0].length);
+
       if (!token['input'] || !/\S/.test(token[0])) continue;
+
+      // concatenate strings
+      if (token['token'] === 'string' && tokens.length > 0 && tokens[tokens.length - 1]['token'] === 'string') {
+        tokens[tokens.length - 1]['string'] = tokens[tokens.length - 1]['string'] + token[0];
+        continue;
+      }
+
       token['input'] && tokens.push({
         string: token[0],
         token: token['token']
       });
     }
+
     return tokens;
   }
 }
--- test/lexer.ts
+++ test/lexer.ts
@@ -6,17 +6,28 @@ const lexer = new Lexer();
 
 describe('tokens', () => {
   it('should analyze normal HTML tags', () => {
-    let html = '<div class="classname">burn it all</div>';
+    let html = `<div show class="classname" style='{ display: none; }'>1 + 1 > 2</div>`;
     let tokens = lexer.analyze(html)[0];
     expect(tokens.length).to.equal(3);
-    expect(tokens[0]['string']).to.equal('<div class="classname">');
-    expect(tokens[1]['string']).to.equal('burn it all');
+    expect(tokens[0]['string']).to.equal(`<div show class="classname" style='{ display: none; }'>`);
+    expect(tokens[1]['string']).to.equal('1 + 1 > 2');
     expect(tokens[2]['string']).to.equal('</div>');
-    html = '<div onClick="() => 1 < 2">burn it all</div>';
+
+    html = '<div onClick="() => 1 < 2">1 + 1 > 2</div>';
     tokens = lexer.analyze(html)[0];
+    expect(tokens.length).to.equal(3);
     expect(tokens[0]['string']).to.equal('<div onClick="() => 1 < 2">');
-    expect(tokens[1]['string']).to.equal('burn it all');
+    expect(tokens[1]['string']).to.equal('1 + 1 > 2');
     expect(tokens[2]['string']).to.equal('</div>');
+
+    html = '>aaa><div>>oh my</div>>';
+    tokens = lexer.analyze(html)[0];
+    expect(tokens.length).to.equal(5);
+    expect(tokens[0]['string']).to.equal('>aaa>');
+    expect(tokens[1]['string']).to.equal('<div>');
+    expect(tokens[2]['string']).to.equal('>oh my');
+    expect(tokens[3]['string']).to.equal('</div>');
+    expect(tokens[4]['string']).to.equal('>');
   });
 
   it('should analyze correct self-closing tags', () => {
@@ -24,6 +35,7 @@ describe('tokens', () => {
     let tokens = lexer.analyze(html)[0];
     expect(tokens.length).to.equal(1);
     expect(tokens[0]['string']).to.equal(html.trim());
+
     html = '<input onClick="function() { 1 < 2 }" />';
     tokens = lexer.analyze(html)[0];
     expect(tokens.length).to.equal(1);
