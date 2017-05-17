6548c64f82411e75b200668410cde1aceeb3830b
hante <hante@youzan.com>
2017-05-17 22:44:57 +0800

    fix: fix parser bugs

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,33 +1,33 @@
 export default class Parser {
   parse(tokens: Array<Array<Object>>) {
-     const toks: Array<Object> = tokens.reduce((tok1, tok2) => tok1.concat(tok2))
-     let root: Node = new Node({ token: 'root', string: '<root ' });
-     let stack: Stack = new Stack();
-     stack.push(root);
+    const toks: Array<Object> = tokens.reduce((tok1, tok2) => tok1.concat(tok2));
+    let root: Node = new Node({ token: 'root', string: '<root ' });
+    let stack: Stack = new Stack();
+    stack.push(root);
 
-     for (let i = 0; i < tokens.length; i += 1) {
-       const token: Object = tokens[i];
-       const type = token['token'];
-       if (type === 'start-tag') {
-         const node = new Node(token);
-         stack.top().addChild(node);
-         stack.push(node);
-       } else if (type === 'end-tag') {
-         const node = new Node(token);
-         if (stack.isEmpty() || node.name !== stack.top().name) {
-           throw new Error('unclosed tag exists');
-         }
-         stack.pop();
-       } else {
-         stack.top().addChild(new Node(token));
-       }
-     }
+    for (let i = 0; i < toks.length; i += 1) {
+      const token: Object = toks[i];
+      const type = token['token'];
+      if (type === 'start-tag') {
+        const node = new Node(token);
+        stack.top().addChild(node);
+        stack.push(node);
+      } else if (type === 'end-tag') {
+        const node = new Node(token);
+        if (stack.isEmpty() || node.name !== stack.top().name) {
+          throw new Error('unclosed tag exists');
+        }
+        stack.pop();
+      } else {
+        stack.top().addChild(new Node(token));
+      }
+    }
 
-     if (!stack.isEmpty()) {
-       throw new Error('unclosed tag exists');
-     }
+    if (!stack.isEmpty()) {
+      throw new Error('unclosed tag exists');
+    }
 
-     return root;
+    return root;
   }
 }
 
@@ -50,7 +50,7 @@ class Stack {
   }
 
   isEmpty() {
-    return this.stack.length === 0;
+    return this.stack.length <= 1;
   }
 
   inspect() {
@@ -66,7 +66,7 @@ class Node {
     this.isString = attrs['token'] === 'string';
     if (!this.isString) {
       this.children = [];
-      this.name = attrs['string'].split(' ')[0].substring(1).toLowerCase();
+      this.name = attrs['string'].split(' ')[0].replace(/[<>/]/g, '').toLowerCase();
     }
   }
 
--- test/parser.ts
+++ test/parser.ts
@@ -1,11 +1,16 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import Lexer from '../src/Lexer';
 import Parser from '../src/Parser';
 
+const lexer = new Lexer();
 const parser = new Parser();
 
 describe('Parser', () => {
-  it('', () => {
-    console.log(parser);
+  it('parse tokens', () => {
+    const html = '<div>cat!</div>';
+    const tokens = lexer.analyze(html);
+    const treeRoot = parser.parse(tokens);
+    console.log(treeRoot);
   })
 });
