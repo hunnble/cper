56f000dcacfc889194a26c6911910094174f43d2
Noski <hante_hunter@163.com>
2019-05-21 20:15:20 +0800

    remove console

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,15 @@ function draw(html) {
   const tokens = lexer.analyze(html);
   const root = parser.parse(tokens);
   const stack = new Stack();
-  console.log(html);
 }
 
 function main() {
   const input = document.getElementById('input');
-  console.log(input);
   input.onchange = (e) => {
     draw(e.target['value']);
   }
 }
 
 window.onload = () => {
-  console.log(111);
   main();
 }
